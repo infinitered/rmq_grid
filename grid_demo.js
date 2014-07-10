afd40b191e4be64eacb6bb99276cc836606a9924
Gant Laborde <gantman@gmail.com>
2014-07-10 14:52:29 -0500

    Update grid_demo.js

--- grid_demo.js
+++ grid_demo.js
@@ -139,7 +139,7 @@ var grid_demo = {
     var attempt_delete = function(event){
       $( "#dialog-confirm" ).dialog({
       resizable: false,
-      height:200,
+      height:230,
       modal: true,
       buttons: {
         "Yes": function() {
